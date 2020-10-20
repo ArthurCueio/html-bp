70ae1591ad5ba02be2ef68349ca0677be2998036
Arthur Coelho <arthurcoelho@pm.me>
2020-10-20 14:43:33 -0300

    Run function

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 #!/usr/bin/env node
-const path = require("path");
-const fs = require("fs");
+const { run } = require("./lib");
 
 // Get args
 const [, , ...args] = process.argv;
 
-// Copy the file to destination
-const templatePath = path.join(__dirname, "template", "index.html");
-
-try {
-  fs.copyFileSync(templatePath, destinationPath);
-  console.log("Files copied. :)");
-} catch (e) {
-  let message = e.toString();
-  if (e.code === "ENOENT") {
-    message = "Error copying files: No such file or directory";
-  }
-
-  console.log(message);
-  process.exit(1);
-}
-
-// Exit
-process.exit();
+run(args);
